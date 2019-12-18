0943ad9043c19ebbb8d45fe41d2ebc024eff70d5
Nathan Ngo <nathanj238@gmail.com>
2019-12-19 00:01:32 +1100

    Completed calendar. Left 1 acceptable warning.

--- src/DayDisplay.js
+++ src/DayDisplay.js
@@ -2,13 +2,14 @@ import React from 'react';
 
 const dayStrings = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
-// This is a class component, convert me to a functional component
-class DayDisplay extends React.Component {
-    render() {
-        // Must display the day of the selected date.
-        // The JS standard library includes a Date class that will be useful
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date
-    }
+function DayDisplay(props) {
+    // Use Date to figure out the day
+    const dateObj = new Date(props.year, props.month, props.day);
+    const dayIndex = dateObj.getDay();
+    const day = dayStrings[dayIndex];
+
+    // Display it
+    return <div> {day} </div>;
 }
 
 export { DayDisplay };
